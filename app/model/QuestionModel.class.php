c135e19708249e25e553277343046006de1658ba
thomas <t@b128.net>
2013-09-16 16:01:17 +0200

    QuestionModel eingerückt

--- app/model/QuestionModel.class.php
+++ app/model/QuestionModel.class.php
@@ -2,21 +2,21 @@
 namespace model;
 
 class QuestionModel extends Model {
-	
-	function __construct() {
-		parent::__construct();
-	}
-	
-	public static function getQuestion($id) {
-	  $sql = '
-	    SELECT *
-	    FROM questions
-	    WHERE
-	      id =  ?';
-		$stmt = DBConnection->getConnection()->prepare($sql);
-		$stmt->bindValue(1, $id, PDO::PARAM_INT);
-		$stmt->execute();
-		return $stmt->fetchAll();
-	}
+  
+  function __construct() {
+    parent::__construct();
+  }
+  
+  public static function getQuestion($id) {
+    $sql = '
+      SELECT *
+      FROM questions
+      WHERE
+        id =  ?';
+    $stmt = DBConnection::getConnection()->prepare($sql);
+    $stmt->bindValue(1, $id, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll();
+  }
 }
 ?>
